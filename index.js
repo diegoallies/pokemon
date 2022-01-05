ca00baaa854c9b5811ec98e08e7462c4cddde2f9
Encrypto27 <diegoallies27@gmail.com>
2022-01-05 12:21:12 +0200

    added buttons

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
+fetch("https://pokeapi.co/api/v2/pokemon/")
+  .then((res) => res.json())
+  .then((pokemonList) => {
 
-fetch("https://pokeapi.co/api/v2/pokemon/charizard")
+  console.log(pokemonList);
+  pokemonList.results.forEach((pokemon) => {
+    document.querySelector("#list").innerHTML +=`
+      <button onclick="getData('${pokemon.url}')">${pokemon.name}</button>
+     `;
+    
+  });
+  
+  });
+
+function getData(url) {
+fetch(url)
 .then((response) => response.json())
 .then((data) => {
     console.log(data);
    
-        document.getElementById("pokemon").innerHTML +=`
+        document.getElementById("pokemon").innerHTML =`
         <div class="card">
   <img src="${data.sprites.front_default}" class="card-img-top">
   <div class="card-body">
       <h3 class="words"> ${data.name}</h3>
-        <p class="words"> Type: ${data.types[0].type.name} , ${data.types[1].type.name}</p>
-        <p class="words"> Ability: ${data.abilities[0].ability.name} , ${data.abilities[1].ability.name} </p>
+        <p class="words"> Type: ${data.types[0].type.name}</p>
+        <p class="words"> Ability: ${data.abilities[0].ability.name}</p>
         <p class="words"> Height: ${data.height} , weight: ${data.weight}</p>
         <p class="words"> Stats: </p>
         <li class="words"> ${data.stats[0].stat.name} : ${data.stats[0].base_stat} </li>
@@ -26,3 +40,4 @@ fetch("https://pokeapi.co/api/v2/pokemon/charizard")
       
         `
     });
+  }
